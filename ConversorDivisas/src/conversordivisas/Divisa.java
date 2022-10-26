0268f1289054b928a7babf6bcaa56c0e303f9160
DAM2Alu10 <210176@iessierraguara.es>
2022-10-26 14:10:43 +0200

    validation

---
+++ ConversorDivisas/src/conversordivisas/Divisa.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conversordivisas;
+
+/**
+ *
+ * @author DAM2Alu10
+ */
+public class Divisa {
+    String nombre;
+    String valor;
+
+    public Divisa() {
+    }
+
+    public Divisa(String nombre, String valor) {
+        this.nombre = nombre;
+        this.valor = valor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+     
+    
+}
