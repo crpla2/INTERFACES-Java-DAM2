b20ed34f58ff80a39ce71de952760e5ac885bf73
DAM2Alu10 <210176@iessierraguara.es>
2022-11-04 10:37:12 +0100

    añado proyecto horarios

---
+++ Nuevo02/src/nuevo02/Nuevo02.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package nuevo02;
+
+/**
+ *
+ * @author DAM2Alu10
+ */
+public class Nuevo02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
