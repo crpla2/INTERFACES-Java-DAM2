0ce89329232a86b7f58d973b2fa06a9038a1cb07
DAM2Alu10 <210176@iessierraguara.es>
2022-11-08 11:09:49 +0100

    añado proyecto horarios

---
+++ Nuevo01/src/clases/ListaHoras.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author DAM2Alu10
+ */
+public class ListaHoras {
+    private ArrayList<Hora> lista;
+    
+   
+    public ListaHoras() {
+        lista= new ArrayList<Hora>();
+    }
+   public void setHora(Hora h){
+       lista.add(h);
+   }
+   public Hora getHora(int h){
+       lista.forEach(Hora->{if(Hora.getHora()==h)
+           return h;});
+       
+   }
+   
+    
+    
+    
+    
+}
